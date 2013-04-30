5f9e77014adbb345eee350258e5464cb9874d275
Isaac Rabinovitch <isaac@picknit.com>
2013-04-30 16:57:34 -0700

    grunt

--- insideaf2/app.rb
+++ insideaf2/app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
+require 'json'
+require 'mysql'
 set :protection, except: :ip_spoofing
-
+creds = (JSON.parse(ENV['VCAP_SERVICES'])['mysql-5.1'].each do |s|
+  break(s) if s['name'] == 'insideaf-mysql'
+end)['credentials']
+database = Mysql::new(creds['host'], creds['user'], creds['password'], creds['name'])
+puts database.inspect
 get '/' do
-  @env = ENV
+  @env = creds
   erb :index
 end
